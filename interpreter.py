ba05e73b0436fe2f99f41cb4f60db7beff0e0413
Mateusz Warzynski <mathew.warzynski@gmail.com>
2020-10-21 01:53:52 +0200

    Make Machine non-deterministic

--- interpreter.py
+++ interpreter.py
@@ -27,20 +27,18 @@ class Transition:
     direction: Direction
 
 @dataclass
+class Position:
+    state: State
+    letter: Letter
+
+@dataclass
 class Move:
     letter_to_write: Letter
     direction: Direction
     state_target: State
 
 
-class TransitionAlreadyExists(Exception):
-    pass
-
-class TransitionAlreadyExists(Exception):
-    pass
-
-
-class Definition:
+class MachineDefinition:
     
     def __init__(self, transitions: [Transition]):
         self._parse_transitions(transitions)
@@ -54,56 +52,76 @@ class Definition:
                 self._definition[t.state_current][t.letter_current] = []
             self._definition[t.state_current][t.letter_current].append(Move(t.letter_to_write, t.direction, t.state_target))
 
-    def evaluate_move(self, state: State, letter_current: Letter) -> Move:
+    def evaluate_moves(self, pos: Position) -> [Move]:
         try:
-            return self._definition[state][letter_current]
+            return self._definition[pos.state][pos.letter]
         except KeyError:
             return None
 
 
+class MachineState:
+
+    def __init__(self, tape: [str], head: int, state: State):
+        self._tape = tape
+        self._head = head
+        self._state = state
+
+    def copy(self):
+        return MachineState(self._tape.copy(), self._head, self._state)
+
+    def state(self) -> State:
+        return self._state
+
+    def position(self) -> Position:
+        return Position(self._state, self._tape[self._head])
+
+    def do_move(self, move: Move):
+        # Update state to destination.
+        self._state = move.state_target
+
+        # Update letter in the Tape.
+        self._tape[self._head] = move.letter_to_write
+
+        # Update head position.
+        if move.direction == DIRECTION_LEFT:
+            if self._head > 0:
+                self._head -= 1
+        elif move.direction == DIRECTION_RIGHT:
+            self._head += 1
+        else: # move.direction == DIRECTION_STAY:
+            pass
+
+        if self._head >= len(self._tape):
+            self._tape = self._tape + [BLANK]
+
+
 class Machine:
 
-    def __init__(self, definition: Definition):
+    def __init__(self, definition: MachineDefinition):
         self._definition = definition
 
     def run(self, steps: int, tape_str: str) -> str:
         tape = list(tape_str)
-        head = 0
-        state = STATE_INIT
+        machines = [MachineState(tape, 0, STATE_INIT)]
         step_i = 0
-
         while step_i < steps:
-            letter = BLANK
-            if len(tape) > head:
-                letter = tape[head]
-
-            moves = self._definition.evaluate_move(state, letter)
-            if moves is None or len(moves) == 0:
-                state = STATE_REJECT
+            new_machines = []
+            for machine in machines:
+                pos = machine.position()
+                moves = self._definition.evaluate_moves(pos)
+                if moves is None or len(moves) == 0:
+                    continue
+                for move in moves:
+                    new_machine = machine.copy()
+                    new_machine.do_move(move)
+                    new_machines.append(new_machine)
+            accepts = len([m for m in new_machines if m.state() == STATE_ACCEPT])
+            rejects = len([m for m in new_machines if m.state() == STATE_REJECT])
+            if accepts != 0 or rejects != 0:
                 break
-            move = moves[0]
-            
-            state = move.state_target
-
-            if head >= len(tape):
-                tape = tape + [" "]
-            
-            tape[head] = move.letter_to_write
-            
-            if move.direction == DIRECTION_LEFT:
-                if head > 0:
-                    head -= 1
-            elif move.direction == DIRECTION_RIGHT:
-                head += 1
-            else: # move.direction == DIRECTION_STAY:
-                pass
-
-            if state in [STATE_REJECT, STATE_ACCEPT]:
-                break
-
+            machines = new_machines
             step_i += 1
-
-        if state == STATE_ACCEPT:
+        if accepts > 0 and rejects == 0:
             return "YES"
         return "NO"
 
@@ -126,7 +144,7 @@ if __name__ == "__main__":
         sys.exit(0)
 
     transitions = parse_transitions(sys.argv[1])
-    definition = Definition(transitions)
+    definition = MachineDefinition(transitions)
     steps = int(sys.argv[2])
     tape = input("")
 
