23ed7ae78036984ed4355453bc0ab20103baa3c0
Mateusz Warzynski <mathew.warzynski@gmail.com>
2020-10-24 19:48:10 +0200

    Fix Machine: detect loops

--- interpreter.py
+++ interpreter.py
@@ -66,6 +66,9 @@ class MachineState:
         self._head = head
         self._state = state
 
+    def __hash__(self):
+        return hash(("".join(self._tape), self._state, self._head))
+
     def copy(self):
         return MachineState(self._tape.copy(), self._head, self._state)
 
@@ -73,13 +76,19 @@ class MachineState:
         return self._state
 
     def position(self) -> Position:
+        if self._head >= len(self._tape):
+            return Position(self._state, BLANK)
         return Position(self._state, self._tape[self._head])
 
     def do_move(self, move: Move):
+        pos = self.position()
+
         # Update state to destination.
         self._state = move.state_target
 
         # Update letter in the Tape.
+        if self._head >= len(self._tape):
+            self._tape = self._tape + [BLANK]
         self._tape[self._head] = move.letter_to_write
 
         # Update head position.
@@ -88,17 +97,21 @@ class MachineState:
                 self._head -= 1
         elif move.direction == DIRECTION_RIGHT:
             self._head += 1
+            if self._head >= len(self._tape):
+                self._tape = self._tape + [BLANK]
         else: # move.direction == DIRECTION_STAY:
             pass
 
-        if self._head >= len(self._tape):
-            self._tape = self._tape + [BLANK]
+        pos_next = self.position()
 
 
 class Machine:
 
+    _states_visited: set()
+
     def __init__(self, definition: MachineDefinition):
         self._definition = definition
+        self._states_visited = set()
 
     def run(self, steps: int, tape_str: str) -> str:
         tape = list(tape_str)
@@ -114,6 +127,9 @@ class Machine:
                 for move in moves:
                     new_machine = machine.copy()
                     new_machine.do_move(move)
+                    if hash(new_machine) in self._states_visited:
+                        continue
+                    self._states_visited.add(hash(new_machine))
                     new_machines.append(new_machine)
             accepts = len([m for m in new_machines if m.state() == STATE_ACCEPT])
             rejects = len([m for m in new_machines if m.state() == STATE_REJECT])
@@ -121,6 +137,8 @@ class Machine:
                 break
             machines = new_machines
             step_i += 1
+            if len(machines) == 0:
+                break
         if accepts > 0 and rejects == 0:
             return "YES"
         return "NO"
@@ -131,7 +149,12 @@ def parse_transitions(filepath: str) -> [Transition]:
     try:
         with open(filepath, "r") as f:
             for line in f.readlines():
-                transitions.append(Transition(*line.replace("\n", "").split(" ")))
+                line = line.replace("\n", "")
+                if len(line.replace(" ", "")) == 0:
+                    continue
+                if len(line) >= 2 and line[:2] == "//":
+                    continue
+                transitions.append(Transition(*line.split(" ")))
     except FileNotFoundError as e:
         print(e)
         sys.exit(1)
